85d8593390cfafabe20f8731505ed99e89fd6d53
CMPNION <norkindima57@gmail.com>
2025-01-17 22:38:00 +0500

    Initial commit :w :q

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Убедитесь, что путь правильный для вашего проекта
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
